149fb98958b05ad1fb3fcd70101a4beea29acdfb
Weibin Xu <weibin.xu@agmsolutions.net>
2023-04-24 22:17:52 +0200

    Disable Apollo GraphQL client caching for watchQuery

--- src/app/shared/modules/graphql.module.ts
+++ src/app/shared/modules/graphql.module.ts
@@ -1,14 +1,22 @@
 import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {ApolloClientOptions, DefaultOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 import {environment} from "../../../environments/environment";
 
 const uri: string = environment.gitlabGraphQlUrl;
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+  },
+}
+
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
     cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
   };
 }
 
